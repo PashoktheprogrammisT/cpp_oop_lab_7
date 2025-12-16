6c0124b305a3b73cbdaf961c3ae9ffa4ab164ed7
bxusha <160039558+PashoktheprogrammisT@users.noreply.github.com>
2025-12-17 01:27:01 +0300

    Update ThreadManager.cpp

--- src/ThreadManager.cpp
+++ src/ThreadManager.cpp
@@ -1,1 +1,27 @@
+#include "ThreadManager.h"
 
+ThreadManager::~ThreadManager() {
+    joinAll();
+}
+
+void ThreadManager::addThread(std::function<void()> task) {
+    threads.emplace_back(task);
+}
+
+void ThreadManager::joinAll() {
+    for (auto& thread : threads) {
+        if (thread.joinable()) {
+            thread.join();
+        }
+    }
+    threads.clear();
+}
+
+void ThreadManager::detachAll() {
+    for (auto& thread : threads) {
+        if (thread.joinable()) {
+            thread.detach();
+        }
+    }
+    threads.clear();
+}
