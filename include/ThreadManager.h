1ac593dfc3f0c8ca9d1b254ca30a3d5c77314dec
bxusha <160039558+PashoktheprogrammisT@users.noreply.github.com>
2025-12-17 01:24:55 +0300

    Update ThreadManager.h

--- include/ThreadManager.h
+++ include/ThreadManager.h
@@ -1,1 +1,21 @@
+#ifndef THREADMANAGER_H
+#define THREADMANAGER_H
 
+#include <vector>
+#include <thread>
+#include <functional>
+
+class ThreadManager {
+private:
+    std::vector<std::thread> threads;
+    
+public:
+    ThreadManager() = default;
+    ~ThreadManager();
+    
+    void addThread(std::function<void()> task);
+    void joinAll();
+    void detachAll();
+};
+
+#endif
