3c18b8d69f89221f6a4a3449fc08c414f44e41ad
bxusha <160039558+PashoktheprogrammisT@users.noreply.github.com>
2025-12-17 01:25:12 +0300

    Update utils.h

--- include/utils.h
+++ include/utils.h
@@ -1,1 +1,16 @@
+#ifndef UTILS_H
+#define UTILS_H
 
+#include "NPC.h"
+#include <random>
+#include <string>
+
+namespace utils {
+    int randomInt(int min, int max);
+    NPCType randomNPCType();  // Случайный тип NPC
+    std::string npcTypeToString(NPCType type);
+    int getMoveDistance(NPCType type);  // Получить расстояние хода по таблице
+    int getKillDistance(NPCType type);  // Получить расстояние убийства по таблице
+}
+
+#endif
