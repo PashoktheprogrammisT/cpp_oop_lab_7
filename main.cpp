05f09bf8cf93fc8675ce169d5f2d419b9e857e56
bxusha <160039558+PashoktheprogrammisT@users.noreply.github.com>
2025-12-17 01:28:46 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
+#include "Game.h"
+#include <iostream>
 
+int main() {
+    std::cout << "=== NPC Battle Simulation (Lab 7) ===\n";
+    std::cout << "Variant 3: Druid (Move: 10, Kill: 10)\n";
+    std::cout << "Game duration: 30 seconds\n";
+    
+    Game game;
+    game.initialize(50);
+    game.start();
+    
+    std::cout << "\n=== Game Over ===\n";
+    return 0;
+}
